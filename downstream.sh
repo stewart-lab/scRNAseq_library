94803d1cd42a3f07231210b4f14f2a2de41817d3
Moore <bmjohns43@gmail.com>
2023-06-02 11:40:52 -0500

    downstream analyis added

---
+++ downstream.sh
@@ -0,0 +1,15 @@
+## QC covariates
+
+# filter out genes only expressed in < 3 cells
+# filter high mitochondrial expressed cells
+# filter ambient RNA (SoupX)
+# Doublet detection (DoubletFinder)
+# Normalization (scran)
+# Feature selection (remove genes with constant expressioin- seurat)
+# dimensionality reduction (PCA, UMAP)
+
+## cluster on preprocessed data
+
+# cluster (seurat-Leiden algorithm)
+
+## cluster annotation
