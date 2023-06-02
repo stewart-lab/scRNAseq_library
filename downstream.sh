4d1731e8e7d170118f0d54260ad73d9dd208fcee
Bethany Moore <bmjohns43@gmail.com>
2023-06-02 11:48:06 -0500

    Update downstream.sh with scDblFinder

--- downstream.sh
+++ downstream.sh
@@ -3,7 +3,7 @@
 # filter out genes only expressed in < 3 cells
 # filter high mitochondrial expressed cells
 # filter ambient RNA (SoupX)
-# Doublet detection (DoubletFinder)
+# Doublet detection (DoubletFinder, scDblFinder: https://bioconductor.org/packages/release/bioc/vignettes/scDblFinder/inst/doc/scDblFinder.html)
 # Normalization (scran)
 # Feature selection (remove genes with constant expressioin- seurat)
 # dimensionality reduction (PCA, UMAP)
