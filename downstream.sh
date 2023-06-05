061c735d85e5d173ab88b9e31c8a3cb5fd4a0734
Jack Freeman <jackwfreeman@yahoo.com>
2023-06-05 13:58:02 -0500

    Update downstream.sh

--- downstream.sh
+++ downstream.sh
@@ -4,7 +4,9 @@
 # filter high mitochondrial expressed cells
 # filter ambient RNA (SoupX)
 # Doublet detection (DoubletFinder, scDblFinder: https://bioconductor.org/packages/release/bioc/vignettes/scDblFinder/inst/doc/scDblFinder.html)
+# Filter out Ribo?
 # Normalization (scran)
+# Filter out cell cycle (regression post Norm)
 # Feature selection (remove genes with constant expressioin- seurat)
 # dimensionality reduction (PCA, UMAP)
 
