1de3c09a70b6f98504cc24908d2868fef436f6bc
John Steill <jsteill@morgridge.org>
2023-09-13 16:12:20 -0500

    abstract, data dirs

---
+++ src/paper.R
@@ -0,0 +1,13 @@
+# variables
+GAMM_FASTQ_DIR <- "/w5home/bmoore/scRNAseq/GAMM/DATA/Orig_Fastqs/"
+PIG_GENOME_DIR <- "/w5home/bmoore/pig_genome/ensemble/Sus_scrofa_genome_forstar_MT/"
+REH_FASTQ_DIR <- "/w5home/bmoore/scRNAseq/GAMM/human_data/reh_cellrep_2020/fastq_files/bam2fastq_out/AS_D205_0_1_H7CLLBGXC/"
+HUMAN_GENOME_DIR <- "/w5home/bmoore/human_genome_38/star_solo_index/"
+
+# install_stewart_sclib()
+
+# Make an empty make_figure_1() function for now
+make_figure_1 <- function() {
+    # Chapter 1: get data (check if it's there, download if needed) and expression (star solo)
+    # Chapter 2: prepocessing steps of pipeline
+}
