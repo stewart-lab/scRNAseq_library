0d656639a967c052b391c2fc0dfe7c5c39351363
Bethany Moore <bmjohns43@gmail.com>
2023-10-17 16:40:52 -0500

    wip

--- tutorials/convert_seurat2anndata.R
+++ tutorials/convert_seurat2anndata.R
@@ -12,7 +12,7 @@ library(SeuratDisk)
 # pbmc3k.final
 
 # read in Seurat object
-gamms2<- readRDS(file = "gamms2_clustifyr.rds")
+gamms2<- readRDS(file = "gamms2_cca_pred.rds")
 
 # metadata
 # add in metadata from previous analysis
@@ -27,9 +27,9 @@ gamms2 <- AddMetaData(gamms2, metadata.gamm)
 # save rds object
 saveRDS(gamms2, file= "gamms2_clustifyr.rds")
 # first save seurat as h5 seurat file
-SaveH5Seurat(gamms2, filename = "gamms2_clustifyr.h5Seurat")
+SaveH5Seurat(gamms2, filename = "gamms2_cca_pred.h5Seurat")
 # then convert to h5ad
-Convert("gamms2_clustifyr.h5Seurat", dest = "h5ad")
+Convert("gamms2_cca_pred.h5Seurat", dest = "h5ad")
 
 # can now be read in by scanpy:
 # import scanpy
