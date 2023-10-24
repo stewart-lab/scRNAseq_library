b2c634b05076872a3b18b2b4efdbeef5c8487d96
Moore <bmjohns43@gmail.com>
2023-10-24 16:20:02 -0500

    rearranging files and adding realtime and fluidigm analysis

--- other_sc/convert_seurat2anndata.R
+++ other_sc/convert_seurat2anndata.R
@@ -7,12 +7,17 @@ library(Seurat)
 use_condaenv("scRNAseq_best")
 
 ## to install SeuratDisk
+# first install hdf5r:
+# conda install -c conda-forge r-hdf5r
+# Next install SeuratDisk:
 # conda install -c conda-forge r-seuratdisk
 # or
 # if (!requireNamespace("remotes", quietly = TRUE)) {
 #   install.packages("remotes")
 # }
 # remotes::install_github("mojaveazure/seurat-disk")
+# note for me conda install -c conda-forge r-seuratdisk did not work, but the github install did
+
 library(SeuratDisk)
 
 setwd("/w5home/bmoore/scRNAseq/GAMM/human_data/reh_cellrep_2020/output_seurat_mapping_20230913_100651_cc/")
