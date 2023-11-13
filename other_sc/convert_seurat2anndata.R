29a1976331620456edb73975470362c24c8308d4
Moore <bmjohns43@gmail.com>
2023-11-13 11:21:39 -0600

    added sessioninfo to scripts to get the R packages and versions used. modified to make more variables

--- other_sc/convert_seurat2anndata.R
+++ other_sc/convert_seurat2anndata.R
@@ -1,11 +1,10 @@
+
 library(reticulate)
 library(purrr)
 library(jsonlite)
 library(rmarkdown)
 library(Seurat)
-
-use_condaenv("scRNAseq_best")
-
+use_condaenv(condaenv = 'scRNAseq_best', required = TRUE)
 ## to install SeuratDisk
 # first install hdf5r:
 # conda install -c conda-forge r-hdf5r
@@ -20,11 +19,11 @@ use_condaenv("scRNAseq_best")
 
 library(SeuratDisk)
 
-setwd("/w5home/bmoore/scRNAseq/GAMM/human_data/reh_cellrep_2020/output_seurat_mapping_20230913_100651_cc/")
+setwd("/w5home/bmoore/scRNAseq/LiFangChu/fluidigm_gup_expr_results/output_20231106_154636/")
 
 
 # read in Seurat object
-gamms2<- readRDS(file = "gamms2_cca_pred.rds")
+seurat.obj<- readRDS(file = "clustered_seurat_obj.rds")
 
 # add in metadata from previous analysis
 # metadata.gamm <- read.csv("../gamm_manual_annot_metadata_c0.5.txt", row.names = 1, 
@@ -37,9 +36,9 @@ gamms2<- readRDS(file = "gamms2_cca_pred.rds")
 
 
 # first save seurat as h5 seurat file
-SaveH5Seurat(gamms2, filename = "gamms2_cca_pred1.h5Seurat")
+SaveH5Seurat(seurat.obj, filename = "clustered_seurat_obj.h5Seurat")
 # then convert to h5ad
-Convert("gamms2_cca_pred1.h5Seurat", dest = "h5ad")
+Convert("clustered_seurat_obj.h5Seurat", dest = "h5ad")
 
 # can now be read in by scanpy:
 # import scanpy
