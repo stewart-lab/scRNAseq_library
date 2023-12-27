f9a142019f97c11475ccbe1d8ad3be18f1cfeee0
Moore <bmjohns43@gmail.com>
2023-12-27 09:25:19 -0600

    adding some downstream updates

--- downstream_sc/convert_seurat2anndata.R
+++ downstream_sc/convert_seurat2anndata.R
@@ -19,26 +19,26 @@ use_condaenv(condaenv = 'scRNAseq_best', required = TRUE)
 
 library(SeuratDisk)
 
-setwd("/w5home/bmoore/scRNAseq/LiFangChu/fluidigm_gup_expr_results/output_20231114_102348/")
+setwd("/w5home/bmoore/scRNAseq/GAMM/GAMM_S2/output_20230830_155530/")
 
 
 # read in Seurat object
-seurat.obj<- readRDS(file = "clustered_seurat_obj.rds")
+gamms2<- readRDS(file = "clustered_seurat_obj.rds")
 
 # add in metadata from previous analysis
-# metadata.gamm <- read.csv("../gamm_manual_annot_metadata_c0.5.txt", row.names = 1, 
-#                           header = TRUE, sep = "\t")
-# # check metadata with query data
-# colnames(gamms2@assays$RNA@data)[1:10]
-# rownames(metadata.gamm)[1:10]
-# # add metadata to query data
-# gamms2 <- AddMetaData(gamms2, metadata.gamm)
+metadata.gamm <- read.csv("../../gamm_metadata/gamm_manual_annot_metadata_c0.5.txt", row.names = 1, 
+                          header = TRUE, sep = "\t")
+# check metadata with query data
+colnames(gamms2@assays$RNA@data)[1:10]
+rownames(metadata.gamm)[1:10]
+# add metadata to query data
+gamms2 <- AddMetaData(gamms2, metadata.gamm)
 
 
 # first save seurat as h5 seurat file
-SaveH5Seurat(seurat.obj, filename = "clustered_seurat_obj.h5Seurat")
+SaveH5Seurat(gamms2, filename = "GAMM_S2_clabeled-clusters_0.5.h5Seurat")
 # then convert to h5ad
-Convert("clustered_seurat_obj.h5Seurat", dest = "h5ad")
+Convert("GAMM_S2_clabeled-clusters_0.5.h5Seurat", dest = "h5ad")
 
 # can now be read in by scanpy:
 # import scanpy
