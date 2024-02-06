191f05bd6d1875469839be83b1131ff3b81f0474
Moore <bmjohns43@gmail.com>
2024-02-06 09:15:28 -0600

    adding realtime analysis, editing some downstream files

--- downstream_sc/convert_seurat2anndata.R
+++ downstream_sc/convert_seurat2anndata.R
@@ -18,27 +18,35 @@ use_condaenv(condaenv = 'scRNAseq_best', required = TRUE)
 # note for me conda install -c conda-forge r-seuratdisk did not work, but the github install did
 
 library(SeuratDisk)
+# variables
+WD <- "/w5home/bmoore/scRNAseq/GAMM/human_data/reh_cellrep_2020/output_seurat_mapping_20231215_092132/"
+SEURAT_OBJ <- "gamms2_cca_pred.rds"
+METADATA_GAMM <- "/w5home/bmoore/scRNAseq/GAMM/gamm_metadata/gammS2_manual_annot_metadata_c0.5.txt"
+OUTPUT_name <- "GAMM_S2_ortho_clabeled-clusters_0.5.h5Seurat"
 
-setwd("/w5home/bmoore/scRNAseq/LiFangChu/fluidigm_gup_expr_results/output_20231114_102348/")
+setwd(WD)
 
 
 # read in Seurat object
-seurat.obj<- readRDS(file = "clustered_seurat_obj.rds")
+gamms2<- readRDS(file = SEURAT_OBJ)
 
 # add in metadata from previous analysis
-# metadata.gamm <- read.csv("../gamm_manual_annot_metadata_c0.5.txt", row.names = 1, 
-#                           header = TRUE, sep = "\t")
-# # check metadata with query data
-# colnames(gamms2@assays$RNA@data)[1:10]
-# rownames(metadata.gamm)[1:10]
-# # add metadata to query data
-# gamms2 <- AddMetaData(gamms2, metadata.gamm)
+metadata.gamm <- read.csv(METADATA_GAMM, row.names = 1,
+                          header = TRUE, sep = "\t")
+# check metadata with query data
+colnames(gamms2@assays$RNA@data)[1:10]
+rownames(metadata.gamm)[1:10]
+# add metadata to query data
+# remove "_1" from metadata
+rownames(metadata.gamm) <- sub("_1", "", rownames(metadata.gamm))
+rownames(metadata.gamm)[1:10]
+gamms2 <- AddMetaData(gamms2, metadata.gamm)
 
 
 # first save seurat as h5 seurat file
-SaveH5Seurat(seurat.obj, filename = "clustered_seurat_obj.h5Seurat")
+SaveH5Seurat(gamms2, filename = OUTPUT_name)
 # then convert to h5ad
-Convert("clustered_seurat_obj.h5Seurat", dest = "h5ad")
+Convert(OUTPUT_name, dest = "h5ad")
 
 # can now be read in by scanpy:
 # import scanpy
