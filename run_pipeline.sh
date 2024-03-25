588a0414e4e81682fa45ca457aa67d6ac586f1e2
Moore <bmjohns43@gmail.com>
2024-03-25 16:13:12 -0500

    working on implementing cross-species, currently permission issues

--- run_pipeline.sh
+++ run_pipeline.sh
@@ -21,21 +21,23 @@ if [ "$confirm" == "y" ] || [ "$confirm" == "Y" ]; then
 
   # Recreate the SHARED_VOLUME directory
   mkdir -p "$SHARED_VOLUME"
+  chmod 777 "$SHARED_VOLUME"
 
   # Your next steps here
   DATA_FLAG="--fastq"
 
   echo "Step 2.2: Building Docker image for alignment"
   # Build the Docker image from the pre_pipeline directory
-  docker build -t align_fastqs ./pre_pipeline
+  docker build --build-arg username=$(id -u):$(id -g) -t align_fastqs ./pre_pipeline
 
   echo "Step 2.3: Running Docker container for alignment"
   # Run the Docker container and execute the STAR.sh script
-  docker run -it\
-    --mount type=bind,source="$DATA_DIR",target=/data \
-    --mount type=bind,source="$SHARED_VOLUME",target=/shared_volume \
-    --mount type=bind,source="$CONFIG_FILE",target=/config.json \
-    align_fastqs /bin/bash -c "conda run -n star_env /bin/bash -c 'cd /src && ./STAR.sh'"
+  docker run -it \
+    --user=$(id -u):$(id -g) \
+    --mount type=bind,source=“$DATA_DIR”,target=/data \
+    --mount type=bind,source=“$SHARED_VOLUME”,target=/shared_volume \
+    --mount type=bind,source=“$CONFIG_FILE”,target=/config.json \
+    align_fastqs /bin/bash -c “conda run -n star_env /bin/bash -c ‘cd /src && ./STAR.sh’”
 else
   echo "Skipped align_fastqs."
   
@@ -74,16 +76,16 @@ cd ..
 shared_volume_dir=$(pwd)/shared_volume
 echo "Step 5: Building Docker image for the next container"
 # Build the Docker image for the next container
-docker build -t my-scrna-seq:bioinfo_latest ./sc_pipeline
+docker build --build-arg username=$(id -u):$(id -g) -t my-scrna-seq:bioinfo_latest ./sc_pipeline
 
 echo "Step 6: Running the main Docker container"
 
 # Run the Docker container with the correct volume mapping
 docker run -it\
-  --mount type=bind,source="$output_dir",target=/scRNA-seq/output \
-  --mount type=bind,source="$shared_volume_dir",target=/scRNA-seq/shared_volume \
-  --mount type=bind,source="./sc_pipeline/src/config.json",target=/scRNA-seq/src/config.json \
-  my-scrna-seq:bioinfo_latest /bin/bash -c "python /scRNA-seq/get_data.py $DATA_FLAG && /root/miniconda/bin/conda run -n scrnaseq Rscript /scRNA-seq/script.R"
+  --mount type=bind,source=“$output_dir”,target=/scRNA-seq/output \
+  --mount type=bind,source=“$shared_volume_dir”,target=/scRNA-seq/shared_volume \
+  --mount type=bind,source=“./sc_pipeline/src/config.json”,target=/scRNA-seq/src/config.json \
+  my-scrna-seq:bioinfo_latest /bin/bash -c “python /scRNA-seq/get_data.py $DATA_FLAG && /root/miniconda/bin/conda run -n scrnaseq Rscript /scRNA-seq/script.R”
 
 echo "Pipeline completed successfully."
 
